c957c33c34450e58a1e48707fd8dd1be8a8b2378
Tuxinal <24763016+tuxinal@users.noreply.github.com>
2022-06-16 01:36:38 +0430

    use bulk endpoints for scanning

--- src/scan.rs
+++ src/scan.rs
@@ -1,8 +1,18 @@
 use ferinth::{structures::version_structs::Version, Ferinth};
-use furse::{structures::file_structs::File, Furse};
-use reqwest::StatusCode;
+use furse::{
+    structures::{
+        file_structs::{File, HashAlgo},
+        fingerprint_structs::FingerprintMatches,
+    },
+    Furse,
+};
 use sha1::{Digest, Sha1};
-use std::{fs, path::Path, sync::Arc};
+use std::{
+    collections::HashMap,
+    fs,
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
 type Result<T> = std::result::Result<T, Error>;
 #[derive(thiserror::Error, Debug)]
@@ -15,60 +25,71 @@ pub enum Error {
     CurseForgeError(#[from] furse::Error),
 }
 
-/// Scan the given `mod_path` and return a Modrinth project ID, a CurseForge mod ID, none, or both
 pub async fn scan(
     modrinth: Arc<Ferinth>,
     curseforge: Arc<Furse>,
-    mod_path: &Path,
-) -> Result<(Option<String>, Option<i32>)> {
-    Ok((
-        get_modrinth_mod_by_hash(modrinth.clone(), mod_path)
-            .await?
-            .map(|version| version.project_id),
-        get_curseforge_mod_by_hash(curseforge.clone(), mod_path)
-            .await?
-            .map(|file| file.mod_id),
-    ))
-}
+    mod_paths: Vec<&Path>,
+) -> Result<HashMap<PathBuf, (Option<Version>, Option<File>)>> {
+    let modrinth_mods = get_modrinth_mods_by_hash(modrinth.clone(), mod_paths.clone()).await?;
+    let curseforge_mods =
+        get_curseforge_mods_by_hash(curseforge.clone(), mod_paths.clone()).await?;
 
-/// Get the version of the mod at `mod_path`
-pub async fn get_modrinth_mod_by_hash(
-    modrinth: Arc<Ferinth>,
-    mod_path: &Path,
-) -> Result<Option<Version>> {
-    let hash = Sha1::default().chain_update(fs::read(mod_path)?).finalize();
-    let result = modrinth
-        .get_version_from_file_hash(&format!("{:x}", hash))
-        .await;
-    match result {
-        Ok(version) => Ok(Some(version)),
-        Err(err) => {
-            if let ferinth::Error::ReqwestError(source) = &err {
-                if Some(StatusCode::NOT_FOUND) == source.status() {
-                    Ok(None)
-                } else {
-                    Err(err.into())
-                }
-            } else {
-                Err(err.into())
+    let mut file_hashes: HashMap<String, &Path> = HashMap::new();
+    for mod_path in &mod_paths {
+        let hash = Sha1::default().chain_update(fs::read(mod_path)?).finalize();
+        file_hashes.entry(format!("{:x}", hash)).or_insert(mod_path);
+    }
+
+    let mut mods: HashMap<PathBuf, (Option<Version>, Option<File>)> = mod_paths
+        .iter()
+        .map(|path| (path.to_path_buf(), (None, None)))
+        .collect();
+
+    for mod_ in modrinth_mods {
+        if let Some(path) = file_hashes.get(&mod_.0) {
+            mods.entry(path.to_path_buf()).or_default().0 = Some(mod_.1);
+        }
+    }
+    for mod_ in curseforge_mods {
+        if let Some(hash) = mod_
+            .hashes
+            .iter()
+            .find(|hash| hash.algo == HashAlgo::Sha1)
+        {
+            if let Some(path) = file_hashes.get_mut(&hash.value) {
+                mods.entry(path.to_path_buf()).or_default().1 = Some(mod_);
             }
-        },
+        }
     }
+    Ok(mods)
 }
 
-/// Get the file of the mod at `mod_path`
-pub async fn get_curseforge_mod_by_hash(
-    curseforge: Arc<Furse>,
-    mod_path: &Path,
-) -> Result<Option<File>> {
-    let bytes = fs::read(mod_path)?;
-    let mut matches = curseforge
-        .get_fingerprint_matches(vec![bytes.into()])
-        .await?
-        .exact_matches;
-    if matches.is_empty() {
-        Ok(None)
-    } else {
-        Ok(Some(matches.swap_remove(0).file))
+pub async fn get_modrinth_mods_by_hash(
+    modrinth: Arc<Ferinth>,
+    mod_paths: Vec<&Path>,
+) -> Result<HashMap<String, Version>> {
+    let mut hashes = vec![];
+    for file in mod_paths {
+        let hash = Sha1::default().chain_update(fs::read(file)?).finalize();
+        hashes.push(format!("{:x}", hash));
     }
+    let versions = modrinth.get_versions_from_hashes(hashes).await?;
+    Ok(versions)
+}
+
+pub async fn get_curseforge_mods_by_hash(
+    curseforge: Arc<Furse>,
+    mod_paths: Vec<&Path>,
+) -> Result<Vec<File>> {
+    let mut file_contents = vec![];
+    for file in mod_paths {
+        file_contents.push(bytes::Bytes::from(fs::read(file)?));
+    }
+    let FingerprintMatches { exact_matches, .. } =
+        curseforge.get_fingerprint_matches(file_contents).await?;
+    let mut files = vec![];
+    for r#match in exact_matches {
+        files.push(r#match.file);
+    }
+    Ok(files)
 }
