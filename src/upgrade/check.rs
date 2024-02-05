dd58837e67623b2fb7750f8eedda40a83af84e1d
atamakahere <git@atamaka.maskmy.id>
2024-02-05 22:23:07 +0530

    Refactor check functions

--- src/upgrade/check.rs
+++ src/upgrade/check.rs
@@ -3,47 +3,6 @@ use ferinth::structures::version::{Version, VersionFile};
 use furse::structures::file_structs::File;
 use octocrab::models::repos::{Asset, Release};
 
-/// Get the latest compatible file from `files`
-pub fn curseforge<'a>(
-    files: &'a mut [File],
-    game_version_to_check: Option<&str>,
-    mod_loader_to_check: Option<&ModLoader>,
-) -> Option<&'a File> {
-    // Make the newest files come first
-    files.sort_unstable_by_key(|file| file.file_date);
-    files.reverse();
-
-    // Immediately select the newest file if check is disabled, i.e. *_to_check is None
-    files.iter().find(|file| {
-        file.game_versions.iter().any(|version| {
-            game_version_to_check.is_none() || version == game_version_to_check.unwrap()
-        }) && file.game_versions.iter().any(|mod_loader| {
-            mod_loader_to_check.is_none()
-                || Ok(mod_loader_to_check.unwrap()) == mod_loader.parse().as_ref()
-        })
-    })
-}
-
-/// Get the latest compatible version and version file from `versions`
-pub fn modrinth<'a>(
-    versions: &'a [Version],
-    game_version_to_check: Option<&str>,
-    mod_loader_to_check: Option<&ModLoader>,
-) -> Option<(&'a VersionFile, &'a Version)> {
-    versions
-        .iter()
-        .find(|version| {
-            // Immediately select the newest file if check is disabled, i.e. *_to_check is None
-            version.game_versions.iter().any(|version| {
-                game_version_to_check.is_none() || version == game_version_to_check.unwrap()
-            }) && version.loaders.iter().any(|mod_loader| {
-                mod_loader_to_check.is_none()
-                    || Ok(mod_loader_to_check.unwrap()) == mod_loader.parse().as_ref()
-            })
-        })
-        .map(|v| (v.get_version_file(), v))
-}
-
 fn is_jar_file(asset_name: &str) -> bool {
     asset_name.contains("jar")
 }
@@ -68,6 +27,46 @@ fn mod_loader_check(mod_loader: Option<&ModLoader>, asset_name: &str) -> bool {
         })
         // select latest asset if mod loader check is disabled
         .unwrap_or(true)
+}
+
+/// Get the latest compatible file from `files`
+pub fn curseforge<'a>(
+    files: &'a mut [File],
+    game_version_to_check: Option<&str>,
+    mod_loader_to_check: Option<&ModLoader>,
+) -> Option<&'a File> {
+    // Sort files in non increasing order
+    files.sort_unstable_by(|file1, file2| file2.file_date.cmp(&file1.file_date));
+
+    // Immediately select the newest file if check is disabled, i.e. *_to_check is None
+    files.iter().find(|file| {
+        file.game_versions.iter().any(|asset| {
+            game_version_check(game_version_to_check, asset)
+                && mod_loader_check(mod_loader_to_check, asset)
+        })
+    })
+}
+
+/// Get the latest compatible version and version file from `versions`
+pub fn modrinth<'a>(
+    versions: &'a [Version],
+    game_version_to_check: Option<&str>,
+    mod_loader_to_check: Option<&ModLoader>,
+) -> Option<(&'a VersionFile, &'a Version)> {
+    versions
+        .iter()
+        .find(|version| {
+            // Immediately select the newest file if check is disabled, i.e. *_to_check is None
+            version
+                .game_versions
+                .iter()
+                .any(|version| game_version_check(game_version_to_check, version))
+                && version
+                    .loaders
+                    .iter()
+                    .any(|mod_loader| mod_loader_check(mod_loader_to_check, mod_loader))
+        })
+        .map(|v| (v.get_version_file(), v))
 }
 
 /// Get the latest compatible asset from `releases`
