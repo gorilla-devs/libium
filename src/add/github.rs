fb38cfc390c6043ac4bf4a16e8f69061f9b9bfd8
atamakahere <git@atamaka.maskmy.id>
2024-02-29 02:19:22 +0530

    Refactor add::github

--- src/add/github.rs
+++ src/add/github.rs
@@ -1,7 +1,36 @@
+use octocrab::models::{repos::Release, Repository};
+
 use crate::{
     config::structs::{Mod, ModIdentifier, ModIdentifierRef, Profile},
     upgrade::mod_downloadable,
 };
+
+fn project_exist(profile: &Profile, repo: &Repository, repo_name: &(String, String)) -> bool {
+    profile.mods.iter().any(|mod_| {
+        mod_.name.to_lowercase() == repo.name.to_lowercase()
+            || ModIdentifierRef::GitHubRepository(&repo_name) == mod_.identifier.as_ref()
+    })
+}
+
+fn contains_jar_asset(releases: &[Release]) -> bool {
+    releases
+        .iter()
+        .flat_map(|r| &r.assets)
+        .any(|a| a.name.ends_with(".jar"))
+}
+
+async fn is_project_compatible(
+    profile: &Profile,
+    releases: &[Release],
+    check_game_version: bool,
+) -> super::Result<bool> {
+    Ok(mod_downloadable::get_latest_compatible_asset(
+        &releases,
+        profile.get_version(check_game_version),
+        profile.get_loader(check_game_version),
+    )
+    .is_some())
+}
 
 /// Check if the repo of `repo_handler` exists, releases mods, and is compatible with `profile`.
 /// If so, add it to the `profile`.
@@ -15,13 +44,16 @@ pub async fn github(
     check_mod_loader: bool,
 ) -> super::Result<String> {
     let repo = repo_handler.get().await?;
-    let repo_name = (repo.owner.clone().unwrap().login, repo.name.clone());
+    let repo_name = (
+        repo.owner
+            .clone()
+            .expect("Owner name not found in git repo")
+            .login,
+        repo.name.clone(),
+    );
 
     // Check if project has already been added
-    if profile.mods.iter().any(|mod_| {
-        mod_.name.to_lowercase() == repo.name.to_lowercase()
-            || ModIdentifierRef::GitHubRepository(&repo_name) == mod_.identifier.as_ref()
-    }) {
+    if project_exist(profile, &repo, &repo_name) {
         return Err(super::Error::AlreadyAdded);
     }
 
@@ -29,21 +61,14 @@ pub async fn github(
         let releases = repo_handler.releases().list().send().await?.items;
 
         // Check if jar files are released
-        if !releases
-            .iter()
-            .flat_map(|r| &r.assets)
-            .any(|a| a.name.ends_with(".jar"))
-        {
+        if !contains_jar_asset(&releases) {
             return Err(super::Error::NotAMod);
         }
 
         // Check if the repo is compatible
-        mod_downloadable::get_latest_compatible_asset(
-            &releases,
-            profile.get_version(check_game_version),
-            profile.get_loader(check_game_version),
-        )
-        .ok_or(super::Error::Incompatible)?;
+        if !is_project_compatible(profile, &releases, check_game_version).await? {
+            return Err(super::Error::Incompatible);
+        }
     }
 
     // Add it to the profile
