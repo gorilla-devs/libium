50f50579e2877148b4713c1d9a92ee35f7d23ad4
atamakahere <git@atamaka.maskmy.id>
2024-02-29 01:59:19 +0530

    Refactor add::curseforge

--- src/add/curseforge.rs
+++ src/add/curseforge.rs
@@ -3,6 +3,35 @@ use crate::{
     upgrade::mod_downloadable,
 };
 
+fn project_exist(profile: &Profile, project: &furse::structures::mod_structs::Mod) -> bool {
+    profile.mods.iter().any(|mod_| {
+        mod_.name.to_lowercase() == project.name.to_lowercase()
+            || ModIdentifier::CurseForgeProject(project.id) == mod_.identifier
+    })
+}
+
+fn distrubution_denied(project: &furse::structures::mod_structs::Mod) -> bool {
+    project.allow_mod_distribution.map_or(false, |b| !b)
+}
+
+fn is_minecraft_mod(project: &furse::structures::mod_structs::Mod) -> bool {
+    project.links.website_url.as_str().contains("mc-mods")
+}
+
+async fn is_project_compatible(
+    curseforge: &furse::Furse,
+    project: &furse::structures::mod_structs::Mod,
+    profile: &Profile,
+    check_game_version: bool,
+) -> super::Result<bool> {
+    Ok(mod_downloadable::get_latest_compatible_file(
+        curseforge.get_mod_files(project.id).await?,
+        profile.get_version(check_game_version),
+        profile.get_loader(check_game_version),
+    )
+    .is_some())
+}
+
 /// Check if the mod of `project_id` exists, is a mod, and is compatible with `profile`.
 /// If so, add it to the `profile`.
 ///
@@ -18,39 +47,34 @@ pub async fn curseforge(
     let project = curseforge.get_mod(project_id).await?;
 
     // Check if project has already been added
-    if profile.mods.iter().any(|mod_| {
-        mod_.name.to_lowercase() == project.name.to_lowercase()
-            || ModIdentifier::CurseForgeProject(project.id) == mod_.identifier
-    }) {
-        Err(super::Error::AlreadyAdded)
+    if project_exist(profile, &project) {
+        return Err(super::Error::AlreadyAdded);
+    }
 
     // Check if it can be downloaded by third-parties
-    } else if Some(false) == project.allow_mod_distribution {
-        Err(super::Error::DistributionDenied)
+    if distrubution_denied(&project) {
+        return Err(super::Error::DistributionDenied);
+    }
 
     // Check if the project is a Minecraft mod
-    } else if !project.links.website_url.as_str().contains("mc-mods") {
-        Err(super::Error::NotAMod)
+    if !is_minecraft_mod(&project) {
+        return Err(super::Error::NotAMod);
+    }
 
     // Check if the project is compatible
-    } else {
-        if perform_checks {
-            mod_downloadable::get_latest_compatible_file(
-                curseforge.get_mod_files(project.id).await?,
-                profile.get_version(check_game_version),
-                profile.get_loader(check_game_version),
-            )
-            .ok_or(super::Error::Incompatible)?;
-        }
-
-        // Add it to the profile
-        profile.mods.push(Mod {
-            name: project.name.trim().to_string(),
-            identifier: ModIdentifier::CurseForgeProject(project.id),
-            check_game_version,
-            check_mod_loader,
-        });
-
-        Ok(project.name)
+    if perform_checks
+        && !is_project_compatible(curseforge, &project, profile, check_game_version).await?
+    {
+        return Err(super::Error::Incompatible);
     }
+
+    // Add it to the profile
+    profile.mods.push(Mod {
+        name: project.name.trim().to_string(),
+        identifier: ModIdentifier::CurseForgeProject(project.id),
+        check_game_version,
+        check_mod_loader,
+    });
+
+    Ok(project.name)
 }
